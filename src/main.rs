09253b74a58ed755068e36a0713e99e2ac10ba43
Sylvan <sylvan@hey.com>
2021-05-11 22:30:33 +0200

    added a TestType enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fmt::Display;
+
+use structopt::StructOpt;
+
+/// Defines that type of test
+#[derive(Debug, StructOpt)]
+enum TestType {
+    /// Selects all tests
+    All,
+}
+
+impl Display for TestType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TestType::All => write!(f, "All tests"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = TestType::from_args();
+    println!("{}", args);
 }
